ae97370a2a26f9b053e646f2faa246cc3b67075a
Ju-hyeon <mycom12317@gmail.com>
2022-02-24 13:20:17 +0900

    .

--- pages/imgupload.tsx
+++ pages/imgupload.tsx
@@ -1,52 +1,62 @@
 import React, { useRef, useState } from "react";
 import Link from "next/link";
+import axios from "axios";
 
-interface Image {
-  id: number;
-  img: string;
-  url: string;
-}
+// interface Image {
+//   id: number;
+//   img: string;
+//   url: string;
+// }
 
 export default function Home() {
   // 이미지를 내 컴퓨터 파일에서 선택을 하여 업로드 버튼을 누르면 이미지가 보여짐
   // 사진을 배열에 저장할 때 미리보기할 url을 함께 생성하여 저장한다
 
   // 사진 업로드 버튼 이벤트 핸들러
-  const imageInput = useRef();
-  const handleClick = () => {
-    imageInput.current.click();
-  };
-
-  // 사진 등록하기 및 미리보기 기능 구현
-  const [imgList, setImgList] = useState<Image[]>([]); // 사진이 저장될 배열
-  // const [preview, setPreview] = useState<Image[]>([]); // 미리보기
-
-  // const uploadImg = (e) => {
-  //   e.preventDefault();
-  //   const formData = new FormData();
-  //   formData.append("file", this.state.selectedFile);
-
-  //   return axios.post("/");
-  //   // setImgFile([{ img: imgFile, id: Date.now() }]);
-  //   // setImgFile([]);
+  // const imageInput = useRef();
+  // const handleClick = () => {
+  //   imageInput.current.click();
   // };
 
-  const imagePreview = () => {
-    return imgList.map((img) => {
-      <div>
-        <img className="photoPreview" src={img.url} />
-      </div>;
-    });
+  // 미리보기 url을 저장
+  const [previewImg, setPreviewImg] = useState(""); // 미리보기
+  const [imgFiles, setImgFiles] = useState(""); // 서버에 전송할 이미지 저장
+
+  // 미리보기 이미지 파일 저장
+  const saveFileImage = (e) => {
+    setPreviewImg(URL.createObjectURL(e.target.files[0]));
+    console.log(previewImg);
   };
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setImgList(e.target.file[0]);
+  // 업로드 이미지 파일 저장
+  const onChange = (e) => {
+    const img = e.target.files[0];
+    const formData = new FormData();
+    formData.append("img", img);
+  };
+  // 파일 삭제
+  // 파일은 삭제 되지만 글씨는 삭제 안됨
+  const deleteFileImage = () => {
+    URL.revokeObjectURL(previewImg);
+    setPreviewImg("");
   };
 
-  const handleSubmit = (id) => (e) => {
-    e.preventDefault();
-  };
+  // const insertImg = (e) => {
+  //   // console.log(e.target.files[0]); // 이미지 들어온거 확인
+  //   let reader = new FileReader();
+  //   if (e.target.files[0]) {
+  //     reader.readAsDataURL(e.target.files[0]);
+  //     setImg([...img, e.target.files[0]]);
+  //   }
+  //   reader.onloadend = () => {
+  //     const previewImgUrl = reader.result;
+  //     // console.log(previewImgUrl); // bash64로 인코딩된 데이터 확인
+  //     if (previewImgUrl) {
+  //       setPreviewImg([...previewImg, previewImgUrl]);
+  //     }
+  //   };
+  // };
+
   return (
     <div>
       <h1 className="bg-red-900 text-center text-white text-2xl py-4">
@@ -60,30 +70,63 @@ export default function Home() {
         </Link>
       </nav>
       <br />
-      <label className="block">
-        <input
-          type="file"
-          accept="image/jpg, image/jpeg, image/png"
-          multiple // 여러 개의 사진 업로드 가능
-          className="block w-full text-sm text-slate-500
-                    file:mr-4 file:py-2 file:px-4
-                    file:rounded-full file:border-0
-                    file:text-sm file:font-semibold
-                    file:bg-violet-50 file:text-violet-700
-                    hover:file:bg-violet-100"
-          onChange={handleChange}
-        />
-      </label>
-      <button
-        className="bg-green-400 rounded text-white p-2"
-        onClick={(e) => {
-          e.preventDefault();
-          // uploadImg();
-        }}
-      >
-        이미지 업로드
-      </button>
+      <>
+        <h1 className="font-bold mb-4">이미지 미리보기</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <div>
+                  {previewImg && (
+                    <img alt="sample" src={previewImg} className="m-auto" />
+                  )}
+                  <div className="items-center justify-center">
+                    <input
+                      name="imgUpload"
+                      type="file"
+                      accept="image/*"
+                      onChange={saveFileImage}
+                    />
+                    <button
+                      className="btn btn-outline btn-error"
+                      onClick={() => deleteFileImage()}
+                    >
+                      삭제
+                    </button>
+                    <button className="btn btn-outline btn-success">
+                      업로드
+                    </button>
+                  </div>
+                  <br />
+                  <ul>
+                    <strong>업로드 이미지</strong>
+                    {/* 버튼을 클릭했을 시 보여야함 */}
+                  </ul>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </>
       <br />
     </div>
   );
+
+  // return (
+  //   <div>
+  //     <input
+  //       type="file"
+  //       accept="image/*"
+  //       onChange={saveImage}
+  //       ref={(refParam) => (inputRef = refParam)}
+  //       style={{ display: "none" }}
+  //     />
+
+  //     <div className="upload-button">
+  //       <button onClick={() => inputRef.click()}>Preview</button>
+  //       <button onClick={deleteImage}>Delete</button>
+  //       <button onClick={sendImage}>Upload</button>
+  //     </div>
+  //   </div>
+  // );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import { useState } from "react";
 import produce from "immer";
+import { classnames, textColor, textDecoration } from "tailwindcss-classnames";
+import create from "zustand";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 interface Todo {
   id: number;
@@ -12,7 +15,11 @@ interface Todo {
 }
 
 export default function IndexPage() {
-  // const [isDisabled, setIsDisabled] = useState(false);
+  const cn = classnames.bind(classnames); // bind를 하지 않으면 classnames 계속 붙여야 함, cn()로 감싸주고 string 표시와 쉼표로 이름을 구분하면 된다
+  const { register, handleSubmit } = useForm<Todo>();
+  const onSubmit: SubmitHandler<Todo> = (data) => {
+    console.log(data);
+  };
   const [userInput, setUserInput] = useState("");
   const [todoList, setTodoList] = useState<Todo[]>([
     {
@@ -33,38 +40,50 @@ export default function IndexPage() {
     },
   ]);
 
-  const [isChecked, setIsChecked] = useState(false); // 체크 여부
-
-  // const [disable, setDisable] = useState(false); // complete 클릭 여부
+  // const [isChecked, setIsChecked] = useState(false); // 체크 여부
 
   const handleChange = (e) => {
     e.preventDefault(); // 어떤 이벤트를 명시적으로 처리하지 않는 경우, 해당 이벤트에 대한 사용자 에이전트의 기본 동작을 실행하지 않도록 한다
     // 기본 동작 방지?
     setUserInput(e.target.value); // 객체에 담겨있는 값을 읽어온다
-    // console.log(userInput)
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setTodoList([
-      {
-        content: userInput,
-        id: Date.now(),
-        disabled: false,
-        done: false,
-        editContent: "",
-        isEditing: false,
-      },
-      ...todoList,
-    ]);
-    setUserInput("");
-  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setTodoList([
+  //     {
+  //       content: userInput,
+  //       id: Date.now(),
+  //       disabled: false,
+  //       done: false,
+  //       editContent: "",
+  //       isEditing: false,
+  //     },
+  //     ...todoList,
+  //   ]);
+  //   setUserInput("");
+  // };
+
+  // content를 생성 후 그 생성한 content는 수정이 안됨
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setTodoList(
+  //     produce((draft) => {
+  //       draft.unshift({
+  //         content: userInput,
+  //         id: Date.now(),
+  //         disabled: false,
+  //         done: false,
+  //         editContent: "",
+  //         isEditing: false,
+  //       });
+  //     })
+  //   );
+  //   setUserInput("");
+  // };
 
   const handleDelete = (id) => {
     const updateArr = todoList.filter((todo) => todo.id !== id);
-    // filter는 주어진 함수 값이 통과하는 새로운 배열을 만든다
-    // indexOf() 메서드는 호출한 string 객체에서 주어진 값과 일치하는 첫 번째 인덱스를 반환한다
-    // console.log(updateArr);
-
     setTodoList(updateArr);
   };
 
@@ -76,10 +95,17 @@ export default function IndexPage() {
     );
   };
 
-  // map()은 실행한 결과를 가지고 새로운 배열을 만든다
-  // const handleChangeEdit = (e) => {
+  // edit이라는 버튼을 클릭하면 isEditing이 true로 바뀌고 editContent에 content의 내용이 들어가야한다
 
-  // Edit 버튼을 누르면 todo.isEditing을 true로 만들어 주면서 div 대신 input이 나오게 변경
+  // const onChangeEditContent = (id) => (e) => {
+  //   setTodoList(
+  //     produce((draft) => {
+  //       const edit = draft.find((todo) => todo.id === id);
+  //       const editContent = e.target.value;
+  //       edit.content = editContent;
+  //     })
+  //   );
+  // };
 
   const onClickEditButton = (id) => (e) => {
     setTodoList(
@@ -112,12 +138,9 @@ export default function IndexPage() {
   //   setIsDisabled(!isDisabled);
   // };
 
-  // complete라는 버튼을 클릭하면 done이 true로 바뀌고 글자색을 회색으로 바꾸기
-  // edit 버튼 비활성화 or 없애기
-
   return (
     <div>
-      <h1 className="bg-red-900 text-center text-white text-2xl py-4">
+      {/* <h1 className="bg-red-900 text-center text-white text-2xl py-4">
         Todo List
       </h1>
       <nav className="bg-gray-500 flex sm:justify-center space-x-4">
@@ -126,36 +149,120 @@ export default function IndexPage() {
             Image Upload
           </button>
         </Link>
-      </nav>
+      </nav> */}
+      <div className="navbar bg-base-100 mb-40 shadow-xl rounded-box">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            {/* <ul
+              // tabindex="0"
+              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul> */}
+            <nav className="flex sm:justify-center space-x-4">
+              <Link href="/imgupload">
+                <button className="rounded-lg px-3 py-2 font-medium hover:bg-slate-100 hover:text-slate-900">
+                  Image Upload
+                </button>
+              </Link>
+            </nav>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <a className="btn btn-ghost normal-case text-xl">Todo List</a>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+        </div>
+      </div>
       <br />
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          className="bg-gray-200 appearance-none border-2 rounded-lg border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          className="input input-bordered input-primary w-full max-w-x bg-gray-200 mb-3 focus:bg-white"
           type="text"
-          value={userInput}
-          onChange={handleChange}
+          // value={userInput}
+          // onChange={handleChange}
+          {...register("content")}
           placeholder="Enter a todo item"
         />
-        <button
-          className="rounded text-gray-100 px-3 py-1 bg-green-500 hover:shadow-inner hover:bg-green-700 transition-all duration-300 float-right"
-          onClick={handleSubmit}
-        >
+        {/* <button className="btn btn-accent float-right" onClick={handleSubmit}>
           Submit
-        </button>
+        </button> */}
+        <button className="btn btn-accent float-right">Submit</button>
       </form>
-      <ul className="my-8 border transition-all duration-500 relative rounded p-2">
+      <ul className="my-20 border transition-all duration-500 relative rounded p-2">
         {todoList.length >= 1
           ? todoList.map((todo) =>
               todo.isEditing ? (
                 <li key={todo.id} className="flex">
                   <input
-                    className="border-2 rounded-lg border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    // className="border-2 rounded-lg border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                    className="input input-bordered input-success w-full max-w-xs"
                     type="text"
                     value={todo.editContent}
                     onChange={onChangeEditContent(todo.id)}
                   />
                   <button
-                    className="rounded text-gray-100 px-3 py-1 bg-green-500 hover:shadow-inner hover:bg-green-700 transition-all duration-300 float-right"
+                    // className="rounded text-gray-100 px-3 py-1 bg-green-500 hover:shadow-inner hover:bg-green-700 transition-all duration-300 float-right"
+                    className="rounded-full btn btn-accent float-right"
                     onClick={onSubmitEdit(todo.id)}
                   >
                     수정하기
@@ -169,13 +276,20 @@ export default function IndexPage() {
 
                     {/* <div className="border-2 rounded-lg px-4 leading-tight">   {todo}
                  </div> */}
-
-                    <div className="border-2 rounded-lg border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight">
+                    <div
+                      className={cn(
+                        "border-2 rounded-lg border-gray-200 w-full py-2 px-4 text-gray-700 leading-tight",
+                        {
+                          "line-through": todo.disabled,
+                        }
+                      )}
+                    >
                       {todo.content}
                     </div>
                     <div className="flex justify-between">
                       <button
-                        className="rounded-full text-gray-100 px-3 py-1 bg-red-500 hover:shadow-inner hover:bg-red-700 transition-all duration-300"
+                        // className="rounded-full text-gray-100 px-3 py-1 bg-red-500 hover:shadow-inner hover:bg-red-700 transition-all duration-300"
+                        className="rounded-full btn btn-error px-3 py-1"
                         onClick={(e) => {
                           e.preventDefault();
                           handleDelete(todo.id);
@@ -185,7 +299,8 @@ export default function IndexPage() {
                       </button>
 
                       <button
-                        className="rounded-full text-gray-100 px-3 py-1 bg-yellow-500 hover:shadow-inner hover:bg-yellow-700 transition-all duration-300"
+                        // className="rounded-full text-gray-100 px-3 py-1 bg-yellow-500 hover:shadow-inner hover:bg-yellow-700 transition-all duration-300"
+                        className="rounded-full btn btn-warning px-3 py-1"
                         type="submit"
                         onClick={onClickEditButton(todo.id)}
                       >
@@ -193,8 +308,11 @@ export default function IndexPage() {
                       </button>
 
                       <button
-                        className="rounded-full text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300
-                        disabled:text-white disabled:bg-gray-500"
+                        // className="rounded-full text-gray-100 px-3 py-1 bg-blue-500 hover:shadow-inner hover:bg-blue-700 transition-all duration-300
+                        // disabled:text-white disabled:bg-gray-500"
+                        className="rounded-full btn btn-info"
+                        // 위의 className안에 있는 것들은 조건에 상관없이 나타나는 것
+                        // disabled가 true이면 글씨가 연해지거나 취소선 생기게 만들기
                         onClick={(e) => {
                           e.preventDefault;
                           alert("미션 완료!!");
